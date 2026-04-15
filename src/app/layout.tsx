1fa97262ded5a760d0bd9edc9df8914c03b2b29b
Amirhossein <amirhossein.abazari@yahoo.com>
2026-04-16 00:03:28 +0330

    [INIT]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import localFont from "next/font/local";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+
+// fonts
+const IranYekan = localFont({
+  src: [
+    {
+      path: "../assets/fonts/IRANYekanX-Regular.woff2",
+    },
+    {
+      path: "../assets/fonts/IRANYekanXVF.ttf",
+    },
+    {
+      path: "../assets/fonts/webfont.woff",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${IranYekan.className}} antialiased border`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
